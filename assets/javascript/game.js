f47c0a68a65719a5799c82c3c6b576d8a5927721
Michael Rosario <michael@elementoinc.com>
2019-02-08 09:58:32 -0500

    Clean code and add comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 
 var user = {}; // the user's character
 var defender = {} // the defender character
-var resetCharacters = $("#characterList").html();
 var n; // level of the user
 
 $("#characterList A").on("click", function(){
@@ -10,83 +9,112 @@ $("#characterList A").on("click", function(){
 	var character = $(this).html();
 
 	// is the player on the ring?
-	if($(".player").html() == "" || Object.keys(user).length === 0){
+	if( Object.keys(user).length === 0 ) {
 		
-		n = 1;
+		n = 1; // level of the player
 
+		// set the player's stats
 		user.name = $(this).attr('data-name');
 		user.health = $(this).attr('data-health');
 		user.power = $(this).attr('data-power');
 		user.counter = $(this).attr('data-counter');
-		$(".player").html(character);
+
+		// put the player to the ring and remove classes added from previous rounds
+		$(".player")
+			.html(character)
+			.removeClass("winner");
+
+		// show, since this could be hidden on player win
+		$(".opponent,.versus").show(); 
+
+		// this class add styling to the character list when oponent is empty
 		$("#characterList").addClass("chooseOponents");
-		$("#instructions").html("Choose the Defender!");
-		$(".opponent,#fightLog").html("");
+		
+		// add instructions to the player and remove classes added from previous rounds
+		$("#instructions")
+			.html("Choose the Defender!")
+			.removeClass("winner");
+
+		// empty previous logs and oponent
+		$(".opponent,#fightLog").empty();
+
+		// hide their selection from the characterList
 		$(this).parent().hide();
 
+
 	// is the defender on the ring?
-	} else if($(".opponent").html() == "" || Object.keys(defender).length === 0 ) {
-		
-		$(".opponent").html(character);
+	} else if( Object.keys(defender).length === 0 ) {
+
+		// set the defender's stats
 		defender.name = $(this).attr('data-name');
 		defender.health = $(this).attr('data-health');
 		defender.power = $(this).attr('data-power');
 		defender.counter = $(this).attr('data-counter');
+		
+		// put the defender to the ring and remove classes added from previous rounds
+		$(".opponent")
+			.html(character)
+			.removeClass("defeated");
+
+		// set the ring class to ready for styling in css
 		$(".theRing").addClass("ready");
+
+		// add disabled styling to the characterList
 		$("#characterList")
 			.removeClass("chooseOponents")
 			.addClass("disable");
-		
-		$(".opponent").removeClass("defeated");
 
+		// hide their selection from the characterList
 		$(this).parent().hide();
 
-		
-
+		// convert the instructions to a button and bind it functionality
 		$("#instructions")
-			.html("Click here to Attack!")
-			.addClass("attack")
+			.html("Click here to Attack!") 	// change text to CTA
+			.addClass("attack")				// add class for styling in CSS
 			.bind("click",function(){
-				console.log("Attack button pressed");
-				console.log(user);
-				console.log(defender);
+				console.log("Attack button pressed");	// Console debug
+				console.log(user);						// The user data
+				console.log(defender);					// The defender data
 
+				// Add the calculations and display
 				$("#fightLog").html(`
 					<br>
 					<strong>${user.name}</strong> attacks <strong>${defender.name}</strong> for ${user.power*n++}.<br>
 					<strong>${defender.name}</strong> counter-attacks <strong>${user.name}</strong> for ${defender.counter}.
 				`);
 
+				// Decrement user and defender health
 				user.health -= defender.counter;
 				defender.health -= user.power*n;
 
+				// Display user and defender health
 				$(".player .health").html(`Health: ${user.health}`);
 				$(".opponent .health").html(`Health: ${defender.health}`);
 
+				// CASE 1 : User dies
 				if(user.health <= 0){
 
 					$("#instructions")
 							.removeClass("attack")
 							.unbind("click");
 
+
+					// CASE 2 : They both die
 					if(defender.health <= 0){
 						$("#instructions")
 							.html(`Game over! <strong>${user.name}</strong> and <strong>${defender.name}</strong> killed each other!<br>Start again by selecting your character.`);
-
+		
 					} else {
 
 						$("#instructions")
-							.html(`Game over! <strong>${defender.name}</strong> defeated you!<br>Start again by selecting your character.`);
+							.html(`Game over! <strong>${defender.name}</strong> defeated you!<br>
+								Start again by selecting your player above.`);
 					}
-					user = {};
-					defender = {};
-					$(".theRing").removeClass("ready");
-					$("#characterList")
-						.removeClass("chooseOponents")
-						.removeClass("disable");
 
-					 $("#characterList :hidden").show();
+					// RESETS
+					resetGame();
 
+				// CASE 3 : Defender dies
 				} else if(defender.health <= 0){
 
 					$("#instructions")
@@ -100,22 +128,36 @@ $("#characterList A").on("click", function(){
 
 					$("#characterList").addClass("chooseOponents").removeClass("disable");
 
+					// All opponents have been defeated
 					if($("#characterList li:visible").length === 0){
 						$("#instructions")
 							.addClass("winner")
-							.html(`CONGRATULATIONS <strong>${user.name}</strong> beat all the opponents!`);
+							.html(`CONGRATULATIONS <strong>${user.name}</strong> beat all the opponents!<br>
+								Start again by selecting your player above.`);
 						$(".opponent,.versus").hide();
 						$(".player").addClass("winner");
+						
+						// RESETS
+						resetGame();
+						
 					}
-
-					
-
 				
-				} 		
-				
+				} 				
 
 			});
 
 	}
 	return false;
 });
+
+
+function resetGame(){
+	user = {};
+	defender = {};
+	$(".theRing").removeClass("ready");
+	$("#characterList")
+		.removeClass("chooseOponents")
+		.removeClass("disable")
+	$("#characterList :hidden").show();
+
+}
